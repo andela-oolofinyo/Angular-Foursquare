b64c3b0f4e233d5b9eae426457d7a7781e785320
Oluwaseye.Olofinyo <oluwaseye.olofinyo@andela.co>
2014-12-19 08:39:31 +0100

    First commit

---
+++ script/app.js
@@ -0,0 +1,32 @@
+var app = angular.module('foursquare', []);
+
+  app.controller('foursquareController', function( $scope, fourSquareServerId ){
+    $scope.results = [];
+
+    $scope.submitForm = function(){
+      fourSquareServerId.getAPI($scope.search).success( function(data){
+
+        if(data.response.venues.length > 0){
+          angular.forEach(data.response.venues, function(users, index){
+
+            $scope.results.push(users);
+            console.log($scope.results);
+          });
+        }
+        else $scope.confirm = "True";
+        console.log($scope.results);
+      });
+    }
+  });
+
+  app.factory('fourSquareServerId', ['$http', function($http){
+    var url = "https://api.foursquare.com/v2/venues/search?client_id=2W3FEHKKUS5VJURDMO0WUV2PFYDGCFGYWPVWPWBHSBR2MRSP&client_secret=MUORUSSSGSCDZRYJMMV3QDJFJ0GTYUVKXHST50IOGLUGFPWI&v=20140806&m=foursquare&near=lagos,lagos";
+    var foursquareItems = {};
+    console.log(url);
+
+    foursquareItems.getAPI = function( searchval ){
+      return $http.get(url, {params: { query: searchval } });
+    };
+    return foursquareItems;
+
+  }]);
